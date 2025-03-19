375fc4cc7f343f135b51c67d53a4a2d39336a603
FlixHC <161723849+FlixHC@users.noreply.github.com>
2025-03-19 22:04:48 +0700

    Update User.java
    Delete comment for use

--- User.java
+++ User.java
@@ -8,8 +8,96 @@ package com.mycompany.world;
  *
  * @author User
  */
+
+//
 public class User {
- static class UserHolder{
+
+ //Eager Singleton (Instance objek akan dibuat saat proses loading class)
+
+
+ /*
+ public class User {
+   private static final User INSTANCE = new User();
+
+    private User(){}
+
+    public static User getInstance(){
+       return INSTANCE;
+    }
+
+    public String getUserNameById(int id){
+       return "isi data cvx";
+    }
+
+    public User getUserById(int id){
+       return new User();
+    }
+ }
+ */
+
+ //Lazy Singleton (Instansi objek akan dibuatkan ketika belum ada instansi, jika sudah ada maka akan memanggil yang sebelumnya)
+
+ /*
+ public class User {
+   private static User user;
+
+   private User(){}
+
+   public static User getInstance(){
+       if(user == null){
+           user = new User();
+       }
+       return user;
+   }
+
+   public String getUserNameById(int id){
+       return "isi data abc";
+   }
+
+   public User getUserById(int id){
+       return new User();
+   }
+}
+*/
+
+ 
+//Thread Safe Singleton (Menggunakan volatile, instance sebagai penampung, dan synchronized untuk menjamin instansi berupa thread-safe
+// dan menjamin pembuatan objek terjadi hanya sekali untuk semua thread
+
+/*
+ public class User {
+  private static volatile User user;
+
+   private User(){}
+
+   public static User getInstance(){
+       if(user == null){
+           synchronized(User.class){
+               if(user == null){
+                   user = new User();
+               }
+           }
+       }
+       return user;
+   }
+
+   public String getUserNameById(int id){
+       return "Isi data dfjk";
+   }
+
+   public User getUserById(int id){
+       return new User();
+   }
+ }
+*/
+
+ 
+ //Bill Pugh Singleton (Menggunakan inner static class untuk menampung objek)
+
+ 
+ /*
+ public class User {
+  static class UserHolder{
        private static final User INSTANCE = new User();
    }
 
@@ -20,10 +108,11 @@ public class User {
    }
 
    public String getUserNameById(int id){
-       return "jlk";
+       return "isi data jlk";
    }
 
    public User getUserById(int id){
        return new User();
    }
 }
+*/
