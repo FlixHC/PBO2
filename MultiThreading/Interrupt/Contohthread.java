fe1552c0ca62daa87d3743cd39079245d7fe778a
FlixHC <161723849+FlixHC@users.noreply.github.com>
2025-03-31 13:24:49 +0700

    Create Contohthread.java

---
+++ MultiThreading/Interrupt/Contohthread.java
@@ -0,0 +1,30 @@
+public class Contohthread {
+
+    public static void main(String[] args) throws InterruptedException {
+        Runnable runnable = () ->{
+            for (int i = 0; i < 10; i++){
+                //Manual Check
+                if(Thread.interrupted()){
+                    return;
+                }
+                System.out.println("Runnable : " + i);
+                try {
+                    Thread.sleep(1000);
+                    } catch (InterruptedException ex){
+                        return;
+                }
+            }
+        };
+            
+        var thread = new Thread(runnable);
+        
+        System.out.println("Program sedang berjalan ... ");
+        thread.start();
+        thread.sleep(4000);
+        thread.interrupt();
+        System.out.println("Menunggu");
+        thread.join();
+        
+        System.out.println("Program telah dijalankan");
+    }
+}
