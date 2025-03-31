4315e3c5ebe4d4862897521f012d18608ec01a57
FlixHC <161723849+FlixHC@users.noreply.github.com>
2025-03-31 13:41:11 +0700

    Create Contohthread.java

---
+++ MultiThreading/Thread State/Contohthread.java
@@ -0,0 +1,14 @@
+public class Contohthread {
+
+    public static void main(String[] args) throws InterruptedException {
+        var thread = new Thread(() -> {
+            System.out.println(Thread.currentThread().getState());
+            System.out.println("Jalan di thread :  " + Thread.currentThread().getName());
+            });
+        thread.setName("Slide 1");
+        System.out.println(thread.getState());
+        thread.start();
+        thread.join();
+        System.out.println(thread.getState());
+    }
+}
